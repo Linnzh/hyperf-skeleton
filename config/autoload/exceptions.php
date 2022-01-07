b2acbf8fbdbfac350f2df3725562a8d0d209b80f
Linnzh <reg.lynnzh@gmail.com>
2022-01-07 15:21:42 +0800

    fix: 错误码为字符串时异常处理器报错

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 return [
     'handler' => [
         'http' => [
-            \Hrb981027\TreasureBag\Exception\Handler\StandardExceptionHandler::class,
             App\Exception\Handler\AppExceptionHandler::class,
         ],
     ],
